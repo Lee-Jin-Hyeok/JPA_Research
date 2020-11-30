6eb18c5a18a29561dca75f80b91a2e415cabfa20
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-30 18:08:27 +0900

    [CREATE] object of mapped school table

---
+++ src/main/java/com/dsm/jpa/domains/domain/School.java
@@ -0,0 +1,50 @@
+package com.dsm.jpa.domains.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "school")
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    public School() {}
+    public School(Long id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
