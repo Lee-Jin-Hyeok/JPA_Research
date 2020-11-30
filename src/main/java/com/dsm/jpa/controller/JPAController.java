4196cefe515127ff0133db98636d8dba2c37dfc9
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-30 19:57:29 +0900

    [ADD] Test Code

--- src/main/java/com/dsm/jpa/controller/JPAController.java
+++ src/main/java/com/dsm/jpa/controller/JPAController.java
@@ -1,13 +1,22 @@
 package com.dsm.jpa.controller;
 
+import com.dsm.jpa.domains.service.JPAService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JPAController {
 
+    private JPAService jpaService;
+
+    @Autowired
+    public JPAController(JPAService jpaService) {
+        this.jpaService = jpaService;
+    }
+
     @GetMapping("/")
     public void testController() {
-
+        jpaService.testService();
     }
 }
