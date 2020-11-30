4a847bce4e7227b031b65cc9471bfda3948e2590
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-30 19:57:55 +0900

    [ADD] Test Code

--- src/main/java/com/dsm/jpa/domains/service/JPAService.java
+++ src/main/java/com/dsm/jpa/domains/service/JPAService.java
@@ -1,8 +1,27 @@
 package com.dsm.jpa.domains.service;
 
+import com.dsm.jpa.domains.domain.School;
+import com.dsm.jpa.domains.repository.SchoolRepository;
+import com.dsm.jpa.domains.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JPAService {
 
+    private SchoolRepository schoolRepository;
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public JPAService(SchoolRepository schoolRepository, StudentRepository studentRepository) {
+        this.schoolRepository = schoolRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    public void testService() {
+        List<School> schools = schoolRepository.findAll();
+        schools.forEach(System.out::println);
+    }
 }
