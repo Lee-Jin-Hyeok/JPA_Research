1a4bc64cff1941a8509198a6a58788972556db1a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-21 14:40:13 +0900

    Clean project.

--- src/main/java/com/dsm/jpa/domains/repository/SchoolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dsm.jpa.domains.repository;
-
-import com.dsm.jpa.domains.domain.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Long> {
-    School findByName(String name);
-}
--- src/main/java/com/dsm/jpa/domains/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dsm.jpa.domains.repository;
-
-import com.dsm.jpa.domains.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
